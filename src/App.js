c1bc05e729f4bff495921e8bdc8eac1cda682037
oscar maldonado-garcia <oscarmmgg1234@gmail.com>
2026-01-13 20:55:03 -0800

    update title for nav bar

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand as={Link} to="/">
-            Oscar's Blog
+            Oscar's Technical Blog
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
